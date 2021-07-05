b69964930c9af9b8de31a9e1af1d8b76fad9bd37
Clayton Landry <clayton.landry@gmail.com>
2021-07-05 12:24:04 -0400

    home/about/layout

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <Layout pageTitle="About Me">
+                <p>Hi there I am the proud owner of Nala, an Animatch rescue who has won the Cutest Dog Award for 12 consecutive years!</p>
+            </Layout>
+        </main>
+    )
+}
+
+export default AboutPage
+
